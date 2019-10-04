403027e3721e6d19dd15d5a60e9f28d28d58069e
Alex Page <alex@alexpage.com.au>
2019-10-04 10:00:44 -0400

    Use head_commit for when there is no push

--- index.ts
+++ index.ts
@@ -9,6 +9,8 @@ const commitMessage = core.getInput('commit-message', {required: true});
 
 const {head_commit: headCommit, repository} = github.context.payload;
 
+console.log(headCommit, repository);
+
 gitHubPages.publish(
 	siteDirectory,
 	{
